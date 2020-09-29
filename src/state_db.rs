faae9548fcf3fe1df74c998d9bd2a4227ef4d6dc
dkf <dannzor@outlook.com>
2020-09-29 11:33:01 +0200

    statedb: replace leveldb with rocksdb + additional tests

--- src/state_db.rs
+++ src/state_db.rs
@@ -1,11 +1,5 @@
-use std::path::Path;
-use std::vec::Vec;
-
-extern crate leveldb;
-use leveldb::options::{Options, WriteOptions, ReadOptions};
-use leveldb::database::Database;
-use leveldb::database::batch::{Batch, Writebatch};
-use leveldb::kv::KV;
+extern crate rocksdb;
+use rocksdb::{DB, Options};
 
 extern crate crypto;
 use self::crypto::sha2::Sha256;
@@ -17,20 +11,19 @@ pub trait KeyValueIO {
     fn new(path: &str) -> StateDB;
 
     // put inserts the key-value pair in the data storage
-    fn put(&mut self, key: i32, value: &[u8]);
+    fn put(&mut self, key: &str, value: &str);
 
     // delete removes the key from the data storage
-    fn delete(&mut self, key: i32);
-
+    fn delete(&mut self, key: &str);
 
     // get_value retrieves the value of the key if it exists. If it is does not
     // it returns a dummy ouput of 0.
-    fn get_value(&self, key: i32) -> Vec<u8>;
+    fn get_value(&self, key: &str) -> String;
 
     // update_root_hash receives a key-value pair and creates a hash
     // based on the existing root hash and this pair to represent a
     // simple and traceable transition of state change
-    fn update_root_hash(&mut self, key: i32, val: &[u8]);
+    fn update_root_hash(&mut self, key: &str, val: &str);
 
     // get_root_hash retrieves the root hash of the state db which represents
     // the freshest and latest change to the db
@@ -39,7 +32,7 @@ pub trait KeyValueIO {
 
 pub struct StateDB {
     // The leveldb database
-    db: Database<i32>,
+    db: DB,
 
     // The hash of all the states. Each state consists of a key-value pair
     // which represents the account and its balance.
@@ -48,10 +41,9 @@ pub struct StateDB {
 
 impl KeyValueIO for StateDB {
     fn new(path: &str) -> StateDB {
-        let mut opts = Options::new();
-        opts.create_if_missing = true;
-        let path_format = Path::new(&path);
-        let database = Database::open(path_format, opts).unwrap();
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        let database = DB::open_default(path).unwrap();
 
         let mut hasher = Sha256::new();
         hasher.input(b"AbrahmChain");
@@ -60,41 +52,46 @@ impl KeyValueIO for StateDB {
         StateDB { db: database, root_hash: hash_out }
     }
 
-    fn put(&mut self, key: i32, val: &[u8]) {
-        let res = write_db(&self.db, key, val);
+    fn put(&mut self, key: &str, val: &str) {
+        if key == "" || val == "" {
+            return;
+        }
+
+        let res = &self.db.put(key, val);
         match res {
             Ok(_) => self.update_root_hash(key, val),
             Err(e) => panic!("write db error: {:?}", e),
         }
     }
 
-    fn delete(&mut self, key: i32) {
-        let res = get_db(&self.db, key);
-        // if the value is empty we don't need to send a delete cmd
-        // since there is no balance-relevant state to change
-        if res == &[0] {
-            return
+    fn delete(&mut self, key: &str) {
+        let bal = self.get_value(key);
+        if bal == "0" {
+            return;
         }
 
-        let ack = del_db(&self.db, key);
+        let ack = &self.db.delete(key);
         match ack {
             Ok(_) => {
-                let empty = vec![0];
-                self.update_root_hash(key, &empty);
+                self.update_root_hash(key, &"0".to_string());
             },
             Err(e) => panic!("write db error: {:?}", e),
         }
     }
 
-    fn get_value(&self, key: i32) -> Vec<u8> {
-        get_db(&self.db, key)
+    fn get_value(&self, key: &str) -> String {
+        let res = &self.db.get(key);
+        match res {
+            Ok(Some(value)) => std::str::from_utf8(&value).unwrap().to_string(),
+            Ok(None) => "0".to_string(),  // dummy output (missing value)
+            Err(e) => panic!("read db error: {:?}", e),
+        }
     }
 
-    fn update_root_hash(&mut self, key: i32, val: &[u8]) {
+    fn update_root_hash(&mut self, key: &str, val: &str) {
         let mut input = self.root_hash.clone();
-        input.push_str(&key.to_string());
-        let val_str: String = val.into_iter().map(|i| i.to_string()).collect::<String>();
-        input.push_str(&val_str);
+        input.push_str(&key);
+        input.push_str(&val);
         let mut new_hash = Sha256::new();
         new_hash.input_str(&input);
 
@@ -104,34 +101,6 @@ impl KeyValueIO for StateDB {
     fn get_root_hash(&self) -> String {
         self.root_hash.clone()
     }
-}
-
-fn get_db(db: &Database<i32>, key: i32) -> Vec<u8> {
-    let read_opts = ReadOptions::new();
-    let query_result = db.get(read_opts, key);
-    match query_result {
-        Ok(data) => {
-            // if entry does not exist, return a dummy output of [0]
-            data.unwrap_or(vec!(0))
-        },
-        Err(e) => panic!("read db error: {:?}", e),
-    }
-}
-
-fn write_db(db: &Database<i32>, key: i32, val: &[u8]) -> Result<(), leveldb::error::Error> {
-    let batch = &mut Writebatch::new();
-    batch.put(key, val);
-    let write_opts = WriteOptions::new();
-    let ack = db.write(write_opts, batch);
-    ack
-}
-
-fn del_db(db: &Database<i32>, key: i32) -> Result<(), leveldb::error::Error> {
-   let batch = &mut Writebatch::new();
-   batch.delete(key);
-   let write_opts = WriteOptions::new();
-   let ack = db.write(write_opts, batch);
-   ack
 }
 
 
@@ -147,28 +116,26 @@ mod tests {
         let root = db.get_root_hash();
 
         // fund account `1` with a balance
-        db.put(1, &[2]);
+        db.put("1", "2");
         let new_root = db.get_root_hash();
         assert_ne!(root, new_root);
-        assert_eq!(db.get_value(1), &[2]);
+        assert_eq!(db.get_value("1"), "2");
         let root = new_root;
 
-        db.put(2, &[3]);
+        db.put("2", "3");
         let new_root = db.get_root_hash();
         assert_ne!(root, new_root);
-        assert_eq!(db.get_value(2), &[3]);
+        assert_eq!(db.get_value("2"), "3");
 
-        db.delete(1);
+        db.delete("1");
         let new_root = db.get_root_hash();
         assert_ne!(root, new_root);
-        assert_eq!(db.get_value(1), &[0]);
+        assert_eq!(db.get_value("1"), "0");
         let root = new_root;
 
-        db.delete(9); // does not exist
+        db.delete("9"); // does not exist
         let new_root = db.get_root_hash();
         assert_eq!(root, new_root);
-
-        teardown();
     }
 
     #[test]
@@ -177,19 +144,17 @@ mod tests {
         let mut db = setup();
         let root = db.get_root_hash();
 
-        db.put(1, &[2]);
+        db.put("1", "2");
         let new_root = db.get_root_hash();
         assert_ne!(root, new_root);
-        assert_eq!(db.get_value(1), &[2]);
+        assert_eq!(db.get_value("1"), "2");
         let root = new_root;
 
         // remove all non-nil balance from account `1`
-        db.delete(1);
+        db.delete("1");
         let new_root = db.get_root_hash();
         assert_ne!(root, new_root);
-        assert_eq!(db.get_value(1), &[0]);
-
-        teardown();
+        assert_eq!(db.get_value("1"), "0");
     }
 
     #[test]
@@ -197,14 +162,12 @@ mod tests {
     fn test_delete_missing_key_expect_no_state_change() {
         let mut db = setup();
 
-        db.put(1, &[2]);
+        db.put("1", "2");
         let root = db.get_root_hash();
         // send a delete cmd to a key which is missing
-        db.delete(5);
+        db.delete("5");
         let new_root = db.get_root_hash();
         assert_eq!(root, new_root);
-
-        teardown();
     }
 
     #[test]
@@ -212,38 +175,54 @@ mod tests {
     fn test_delete_key_with_no_balance_expect_no_state_change() {
         let mut db = setup();
 
-        db.put(1, &[0]);
+        db.put("1", "0");
         let root = db.get_root_hash();
         // send delete cmd to a key which has no balance
-        db.delete(1);
+        db.delete("1");
         let new_root = db.get_root_hash();
         assert_eq!(root, new_root);
+    }
 
-        teardown();
+    #[test]
+    #[serial]
+    fn test_add_large_key_expect_state_change() {
+        let mut db = setup();
+
+        let genesis_root = db.get_root_hash();
+        db.put("99999999999999999999", "10000000000000000000");
+        let root = db.get_root_hash();
+        assert_ne!(genesis_root, root);
+    }
+
+    #[test]
+    #[serial]
+    fn test_empty_key_expect_no_state_change() {
+        let mut db = setup();
+        let genesis_root = db.get_root_hash();
+
+        db.put("", "1");
+        let root = db.get_root_hash();
+        assert_eq!(genesis_root, root);
+    }
+
+    #[test]
+    #[serial]
+    fn test_empty_value_expect_no_state_change() {
+        let mut db = setup();
+        let genesis_root = db.get_root_hash();
+
+        db.put("1", "");
+        let root = db.get_root_hash();
+        assert_eq!(genesis_root, root);
     }
 
     fn setup() -> StateDB {
         let mut tmp_path: String = env!("CARGO_MANIFEST_DIR", "missing cargo manifest").to_string();
         tmp_path.push_str("/test");
-        let dir = std::fs::create_dir(&tmp_path);
-        match dir {
-            Ok(_) => (),
-            Err(e) => panic!("could not create temporary dir: {:?}", e),
-        }
 
-        let db = StateDB::new(&tmp_path);
-        db
+        let _rmd = std::fs::remove_dir_all(&tmp_path);
+        let _crd = std::fs::create_dir(&tmp_path);
+
+        StateDB::new(&tmp_path)
     }
-
-    fn teardown() {
-        let mut tmp_path: String = env!("CARGO_MANIFEST_DIR", "missing cargo manifest").to_string();
-        tmp_path.push_str("/test");
-        let ack = std::fs::remove_dir_all(&tmp_path);
-        match ack {
-            Ok(_) => (),
-            Err(e) => panic!("could not remove temporary dir: {:?}", e),
-        }
-    }
-
-
 }
