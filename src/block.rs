5a110902e7c3c55e5fdc1da95e8e5978dc3cfe42
dkf <dannzor@outlook.com>
2021-04-19 14:08:22 +0200

    core: update block and add append_block (to chain) fn

--- src/block.rs
+++ src/block.rs
@@ -2,10 +2,13 @@ use crate::swiss_knife::helper;
 use chrono::prelude::*;
 use std::fmt::{Display, Formatter};
 
-#[derive(Clone, Debug)]
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Clone, Copy, Debug)]
 pub struct Block {
     // The identifier of this block
-    hash: String,
+    this_hash: String,
     // The previous block's hash
     previous_hash: String,
     // The time this block was mined
@@ -16,9 +19,14 @@ pub struct Block {
 
 impl Block {
     #[allow(dead_code)]
-    pub fn new(hash: String, previous_hash: String, timestamp: i64, data: &'static str) -> Self {
+    pub fn new(
+        this_hash: String,
+        previous_hash: String,
+        timestamp: i64,
+        data: &'static str,
+    ) -> Self {
         Self {
-            hash,
+            this_hash,
             previous_hash,
             timestamp,
             data,
@@ -28,7 +36,7 @@ impl Block {
     #[allow(dead_code)]
     pub fn genesis(anchor_str: &str) -> Self {
         Self {
-            hash: helper::generate_hash_from_input(anchor_str),
+            this_hash: helper::generate_hash_from_input(anchor_str),
             previous_hash: "".to_string(),
             timestamp: Utc::now().timestamp_millis(),
             data: "InitBlock",
@@ -36,7 +44,27 @@ impl Block {
     }
 
     pub fn hash(&self) -> &str {
-        &self.hash
+        &self.this_hash
+    }
+
+    #[allow(dead_code)]
+    pub fn set_hash(&mut self, plain_text: &str) {
+        let s = helper::generate_hash_from_input(plain_text);
+        // we enable a shared ownership of hash_out ..
+        let hash_out = Rc::new(RefCell::new(s));
+        // and allow `s` to perform runtime borrow checking ..
+        let s = hash_out.clone();
+        // final transformation
+        let s1 = s.borrow().to_string();
+        self.this_hash = s1;
+    }
+
+    pub fn set_prev_hash(&mut self, plain_text: &str) {
+        let s = helper::generate_hash_from_input(plain_text);
+        let hash_out = Rc::new(RefCell::new(s));
+        let s = hash_out.clone();
+        let s1 = s.borrow().to_string();
+        self.previous_hash = s1;
     }
 
     pub fn previous_hash(&self) -> &str {
@@ -80,14 +108,14 @@ mod tests {
         let expected_root_hash = hash_out("0x");
         let expected_previous_hash = "";
         let expected_block_data = "InitBlock";
-        assert_eq!(block.hash, expected_root_hash);
-        assert_eq!(block.previous_hash, expected_previous_hash);
-        assert_eq!(block.data, expected_block_data);
+        assert_eq!(block.hash(), expected_root_hash);
+        assert_eq!(block.previous_hash(), expected_previous_hash);
+        assert_eq!(block.data(), expected_block_data);
     }
 
     #[test]
     fn test_custom_block() {
-        let block = Block::new(
+        let mut block = Block::new(
             hash_out("0x1"),
             hash_out("0x"),
             Utc::now().timestamp_millis(),
@@ -96,8 +124,13 @@ mod tests {
         let expected_root_hash = hash_out("0x1");
         let expected_previous_hash = hash_out("0x");
         let expected_block_data = String::from("data1");
-        assert_eq!(block.hash, expected_root_hash);
-        assert_eq!(block.previous_hash, expected_previous_hash);
-        assert_eq!(block.data, expected_block_data);
+        assert_eq!(block.hash(), expected_root_hash);
+        assert_eq!(block.previous_hash(), expected_previous_hash);
+        assert_eq!(block.data(), expected_block_data);
+
+        // change `current_hash`
+        block.set_hash("0xa");
+        let expected_new_hash = hash_out("0xa");
+        assert_eq!(block.hash(), expected_new_hash);
     }
 }
--- src/core.rs
+++ src/core.rs
@@ -4,13 +4,6 @@ use crate::state_db::StateDB;
 use crate::txn_pool::create_new_tx_pool;
 use crate::txn_pool::TxPool;
 
-extern crate crypto;
-use self::crypto::digest::Digest;
-use self::crypto::sha2::Sha256;
-
-use std::cell::RefCell;
-use std::rc::Rc;
-
 #[allow(dead_code)]
 pub struct Blockchain {
     chain: std::vec::Vec<Block>,
@@ -19,33 +12,32 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
+    // new initializes the blockchain which contains the:
+    // (1) link of blocks [vector],
+    // (2) transaction pool,
+    // (3) backend with user balances
     #[allow(dead_code)]
-    pub fn new(account_db_path: &str) -> Self {
-        // we enable a shared ownership of hash_out ..
-        let hash_out = Rc::new(RefCell::new(create_hash()));
-        // and allow `ss` to perform runtime borrow checking ..
-        let ss = hash_out.clone();
-        // to use this dummy hash in the first block
-        let block = Block::new(
-            ss.borrow().to_string(),
-            ss.borrow().to_string(),
-            0,
-            0,
-            "InitBlock",
-        );
+    pub fn new(genesis_block: Block, account_db_path: &str) -> Self {
         let mut chain_of_blocks = std::vec::Vec::<Block>::new();
-        chain_of_blocks.push(block);
+        chain_of_blocks.push(genesis_block);
         Self {
             chain: chain_of_blocks,
             pool: create_new_tx_pool("0x", true),
             account_db: StateDB::new(account_db_path),
         }
     }
-}
 
-fn create_hash() -> String {
-    let mut hasher = Sha256::new();
-    hasher.input(b"0x123");
-    let hash_out = hasher.result_str();
-    hash_out
+    // append_block appends a block `b` which is assumed to have:
+    // (1) a block hash `this_hash`,
+    // (2) a local creation timestamp,
+    // (3) additional block data.
+    // The function then proceeds to link the current latest block's hash
+    // to `b` through its field `previous_hash`
+    #[allow(dead_code)]
+    pub fn append_block(&mut self, b: Block) {
+        let prev = self.chain.last().copied().unwrap();
+        let prev_hash = prev.hash();
+        b.set_prev_hash(prev_hash);
+        self.chain.push(b);
+    }
 }
