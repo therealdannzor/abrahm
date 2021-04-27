10df125bd043732cdaa047c789c7116ef9440982
dkf <dannzor@outlook.com>
2021-04-27 16:09:25 +0200

    pool manager: impl and tests for send/receive

--- src/pool_manager.rs
+++ src/pool_manager.rs
@@ -1,6 +1,8 @@
 use super::block::Block;
-use super::transaction::Transaction;
+use crate::swiss_knife::helper;
 use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{mpsc, Mutex};
+use std::thread;
 
 /// PoolManager manages the inbound and outbound messages which changes the state
 /// of the transaction pool. The outbound communication is conducted through the
@@ -15,7 +17,7 @@ struct PoolManager {
     // heard from either the p2p network or from its internal consensus engine in
     // regards to the latest gossip about a new block or its latest update on any
     // finalized (committed) blocks
-    last_updated: i64,
+    last_updated: Mutex<i64>,
 
     // rx is a receive channel which listens to p2p messages with proposed blocks.
     // These are unwrapped into transactions when delivered to the tx pool.
@@ -23,33 +25,124 @@ struct PoolManager {
 
     // tx is a sender channel which transmits unconfirmed and pending transactions
     // to the local and shared tx pool, respectively
-    tx: Sender<Transaction>,
+    tx: Sender<Block>,
+}
+
+#[allow(dead_code)]
+impl PoolManager {
+    pub fn new() -> Self {
+        let (tx, rx): (Sender<Block>, Receiver<Block>) = mpsc::channel();
+        Self {
+            last_updated: Mutex::new(helper::new_timestamp()),
+            rx,
+            tx,
+        }
+    }
+
+    pub fn last_time(&self) -> i64 {
+        *self.last_updated.lock().unwrap()
+    }
+
+    pub fn get_new_block(&self) -> Block {
+        // update timestamp of the latest try to fetch a block
+        {
+            update_mutex_timestamp(&self.last_updated);
+        }
+
+        // use the non-blocking and optimistic check to fetch
+        let rec = self.rx.try_recv();
+        let block: Block;
+        match rec {
+            Ok(b) => block = b,
+            Err(_) => panic!("error when fetching block"),
+        }
+
+        block
+    }
+
+    pub fn send_block(&self, b: Block) {
+        {
+            update_mutex_timestamp(&self.last_updated);
+        }
+
+        // copy the tx endpoint
+        let thread_tx = self.tx.clone();
+
+        // use standard threads (not the same as lightweight green threads)
+        let child = thread::spawn(move || {
+            thread_tx.send(b).unwrap();
+        });
+
+        // wait for work to complete
+        child.join().expect("error: child thread panicked");
+    }
+}
+
+fn update_mutex_timestamp(m: &Mutex<i64>) {
+    let mut t = m.lock().unwrap();
+    *t = helper::new_timestamp();
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::block::Block;
 
-    // tests external p2p messages (network layer) that are inbound [pool manager task #1]
-    #[test]
-    fn test_receive_blocks_and_deliver_unconfirmed_txs_to_pool() {
-        assert_eq!(true, false);
+    fn setup() -> (Vec<Block>, PoolManager) {
+        // instead of intializing the whole tx pool struct together with
+        // all its sub components, we abstract it as a vector of blocks to
+        // focus on the channel functionalities (which is the system under
+        // test in this case)
+        let pool = Vec::<Block>::new();
+        let manager = PoolManager::new();
+        (pool, manager)
     }
 
-    // tests internal worker messages (consensus layer) [pool manager task #2]
-    #[test]
-    fn test_deliver_confirmed_blocks_as_txs_to_pool() {
-        assert_eq!(true, false);
+    fn generate_x_blocks(x: u8) -> Vec<Block> {
+        let mut res = Vec::<Block>::new();
+        for i in 1..x + 1 {
+            let b = Block::new(
+                i.to_string(),
+                (i - 1).to_string(),
+                helper::new_timestamp(),
+                "data",
+            );
+            res.push(b);
+        }
+        res
     }
 
-    // tests both external and internal messages [pool manager tasks #1 and #2]
+    // mocks inbound messages and tests the send & receive channels [pool manager task #1 and #2]
     #[test]
-    fn test_deliver_high_amount_of_concurrent_messages_to_pool() {
-        assert_eq!(true, false);
+    fn receives_blocks_and_delivers_to_pool() {
+        let (_, manager) = setup();
+        let blocks_to_send = generate_x_blocks(3);
+        let amount_blocks = blocks_to_send.len();
+        let mut responses = Vec::<Block>::new();
+
+        for i in 0..amount_blocks {
+            manager.send_block(blocks_to_send[i].clone());
+        }
+
+        for _ in 0..amount_blocks {
+            let block = manager.get_new_block();
+            responses.push(block);
+        }
+
+        let b1 = responses[0].clone();
+        let b2 = responses[1].clone();
+        let b3 = responses[2].clone();
+        assert_eq!(b1.hash(), blocks_to_send[0].hash());
+        assert_eq!(b1.previous_hash(), blocks_to_send[0].previous_hash());
+        assert_eq!(b2.hash(), blocks_to_send[1].hash());
+        assert_eq!(b2.previous_hash(), blocks_to_send[1].previous_hash());
+        assert_eq!(b3.hash(), blocks_to_send[2].hash());
+        assert_eq!(b3.previous_hash(), blocks_to_send[2].previous_hash());
     }
 
     // tests that outbound external messages are received properly [pool manager task #3]
     #[test]
-    fn test_broadcast_confirmed_txs_as_blocks_to_other_peers() {
+    fn broadcasts_confirmed_txs_as_blocks_to_other_peers() {
         assert_eq!(true, false);
     }
 }
