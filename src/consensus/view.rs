43b119919b8441a1c5e148ef40890fca5ad8c3da
dkf <dannzor@outlook.com>
2021-05-31 18:42:31 +0200

    consensus: create logic for O-set in new-view message

--- src/consensus/view.rs
+++ src/consensus/view.rs
@@ -1,17 +1,18 @@
 #![allow(unused)]
 
 use super::common::{SequenceNumber, View};
-use super::state::M;
+use super::state::{State, M};
 use std::vec::Vec;
 
 /// Section 4.4 View Changes
 
 // Message format: ⟨VIEW-CHANGE, v+1, n, C, P, i⟩_{σ_i} (signed by replicas)
 pub struct ViewChangeMessage {
-    // The identity of the replica
+    // Replica identity
     i: String,
 
-    // The new view the replica proposes to move the system into; v+1
+    // The new view the replica proposes to move the system into; v+1, where v is
+    // the current view
     next_view: View,
 
     // The sequence number of the last stable checkpoint known to the replica
@@ -64,13 +65,13 @@ pub struct P {
     // A valid pre-preprepare message (without the corresponding request)
     valid_preprepare: Option<M>,
     // 2F matching pre-prepare messages from other unique replicas.
-    matching_preprepares: Vec<M>,
+    matching_prepares: Vec<M>,
 }
 impl P {
     pub fn new() -> Self {
         Self {
             valid_preprepare: None,
-            matching_preprepares: Vec::new(),
+            matching_prepares: Vec::new(),
         }
     }
 
@@ -79,8 +80,95 @@ impl P {
     }
 
     pub fn insert_matching(&mut self, m: M) {
-        self.matching_preprepares.push(m);
+        self.matching_prepares.push(m);
     }
+}
+
+struct BigO {
+    // From the set V, the primary creates a new pre-prepare message for the next view v+1
+    // for each sequence number `n` between `min-s` and `max-s`.
+    // `min-s`: the sequence number for the latest stable checkpoint in V
+    // `max-s`: the sequence number for the highest sequence number in a pre-prepare message in V
+    new_view_preprepares: Vec<M>,
+}
+impl BigO {
+    fn new(new_view_preprepares: Vec<M>) -> Self {
+        Self {
+            new_view_preprepares,
+        }
+    }
+}
+
+fn determine_min_max_s(big_v: Vec<ViewChangeMessage>) -> (SequenceNumber, SequenceNumber) {
+    if big_v.len() < 1 {
+        panic!("cannot determine min-max-s due to empty view change log");
+    }
+
+    let mut min_s = u64::MIN;
+    let mut max_s = u64::MIN;
+
+    for (i, val) in big_v.iter().enumerate().clone() {
+        if val.n > min_s {
+            min_s = val.n.clone();
+        } else if val.big_p[i].matching_prepares[i].n > max_s {
+            max_s = val.big_p[i].matching_prepares[i].n;
+        } else {
+            log::debug!("couldn't find either min_s or max_s in vc vector");
+        }
+    }
+
+    (min_s, max_s)
+}
+
+// From a vector-of-vectors (a vector P containing vector P_m's), we extract a single vector with
+// updated view (the next one) and
+fn new_preprepare_message(
+    big_p: Vec<P>,
+    min_s: SequenceNumber,
+    max_s: SequenceNumber,
+    i: String,
+) -> Vec<M> {
+    let mut res = Vec::<M>::new();
+    // There are two cases for how to create the next preprepare message based on the set V:
+    //   (1) there is a P with a sequence number `n` s.t. min_s <= n <= max_s.
+    //       In this case, increment the view and keep the rest of the message.
+    //   (2) there is not a P with a sequence number `n` within this range.
+    //       In this case, increment the view and modify the message digest
+    //       to the one of a special null request.
+
+    for iter in big_p.iter().clone() {
+        // the valid preprepare should contain the exact same information as in matching prepares
+        if iter.valid_preprepare.clone().unwrap().n >= min_s
+            && iter.valid_preprepare.clone().unwrap().n <= max_s
+        {
+            let case_1_message = inc_v(
+                iter.valid_preprepare.clone().unwrap().i.clone(),
+                iter.valid_preprepare.clone().unwrap().v.clone(),
+                iter.valid_preprepare.clone().unwrap().n.clone(),
+                iter.valid_preprepare.clone().unwrap().d.clone(),
+            );
+            res.push(case_1_message);
+        } else {
+            let case_2_message = inc_v_and_no_op(
+                iter.valid_preprepare.clone().unwrap().i.clone(),
+                iter.valid_preprepare.clone().unwrap().v.clone(),
+                iter.valid_preprepare.clone().unwrap().n.clone(),
+            );
+            res.push(case_2_message);
+        }
+    }
+
+    res
+}
+
+// Creates a new PREPREPARE message with an incremented view
+fn inc_v(i: String, v: View, n: SequenceNumber, d: String) -> M {
+    M::new(State::new(1), i, v + 1, n, d)
+}
+
+// Creates a new PREPREPARE message with an incremented view and a special null request (no-op)
+fn inc_v_and_no_op(i: String, v: View, n: SequenceNumber) -> M {
+    M::new(State::new(1), i, v + 1, n, String::from("NOOP"))
 }
 
 // Message format: ⟨NEW-VIEW, v+1, V, O⟩_{σ_P} (signed by primary)
@@ -92,9 +180,15 @@ pub struct NewViewMessage {
     // its own already sent (or to be sent) message.
     big_v: Vec<ViewChangeMessage>,
 
-    // A set O with preprepares computed in a particular way (see impl).
+    // A set O with preprepares computed in a particular way.
     big_o: BigO,
 }
-
-struct BigO {}
-impl BigO {}
+impl NewViewMessage {
+    pub fn new(v: View, big_v: Vec<ViewChangeMessage>) -> Self {
+        Self {
+            v,
+            big_v: Vec::new(),
+            big_o: BigO::new(Vec::new()),
+        }
+    }
+}
