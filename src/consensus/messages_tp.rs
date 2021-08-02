341f41d1f46b21527b11417643a20041f0d4b8f0
dkf <dannzor@outlook.com>
2021-08-02 11:17:18 +0200

    consensus: enable message types de/serialization

--- src/consensus/messages_tp.rs
+++ src/consensus/messages_tp.rs
@@ -1,8 +1,10 @@
 #![allow(unused)]
 use crate::consensus::common::{Committer, SequenceNumber, View};
+use serde::{Deserialize, Serialize};
 
 ///! Contains the message types for the three-phase consensus (PREPREPARE, PREPARE, COMMIT)
 
+#[derive(Serialize, Deserialize, Clone)]
 // Preprepare orders requests in the same view despite a faulty primary
 pub struct Preprepare {
     v: View,
@@ -29,6 +31,7 @@ impl Preprepare {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone)]
 // Prepare ensures that preprepare messages (requests) that commit are totally ordered across views
 pub struct Prepare {
     v: View,
@@ -55,6 +58,7 @@ impl Prepare {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone)]
 // Commit finalizes the proposal request and starts the commit phase
 pub struct Commit {
     v: View,
