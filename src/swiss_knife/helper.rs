05a6661accbda7bcb0b87d6628da34fe23dbbd20
dkf <dannzor@outlook.com>
2021-08-04 15:17:45 +0200

    helper: add key file i/o aux functions

--- src/swiss_knife/helper.rs
+++ src/swiss_knife/helper.rs
@@ -1,7 +1,9 @@
 use chrono::prelude::Utc;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
-use std::io::Write;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines, Write};
+use themis::keys::EcdsaPublicKey;
 
 pub fn write_file<K: AsRef<[u8]>>(key: K, path: &str) -> std::io::Result<()> {
     // no matter where we are in the project folder, always save the keys in the same
@@ -10,6 +12,56 @@ pub fn write_file<K: AsRef<[u8]>>(key: K, path: &str) -> std::io::Result<()> {
     let mut file = std::fs::File::create(new_path)?;
     file.write_all(key.as_ref())?;
     Ok(())
+}
+
+pub fn read_file_by_lines(path: &str) -> Result<Vec<EcdsaPublicKey>, std::io::Error> {
+    let mut keys = Vec::new();
+    if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            if line.is_err() {
+                return Err(line.err().unwrap());
+            }
+            let line = line.unwrap();
+            let key = is_string_valid_ecdsa(line);
+            if key.is_err() {
+                return Err(key.err().unwrap());
+            }
+            let key = key.unwrap();
+            keys.push(key);
+        }
+        if keys.len() < 4 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "found less than four validators",
+            ));
+        }
+        return Ok(keys);
+    } else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "couldn not read lines",
+        ));
+    }
+}
+
+fn read_lines<P>(filename: P) -> std::io::Result<Lines<BufReader<File>>>
+where
+    P: AsRef<std::path::Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
+
+pub fn is_string_valid_ecdsa(key: String) -> Result<EcdsaPublicKey, std::io::Error> {
+    let key: &[u8] = key.as_ref();
+    let maybe_valid = themis::keys::EcdsaPublicKey::try_from_slice(key);
+    if maybe_valid.is_err() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "string is not a valid EcdsaPublicKey",
+        ));
+    }
+    Ok(maybe_valid.unwrap())
 }
 
 pub fn remove_suffix<'a>(s: &'a &str, p: &str) -> &'a str {
