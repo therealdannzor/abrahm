07f72d35d9c53ca115be4b89f9ca6df34da7a882
dkf <dannzor@outlook.com>
2021-09-02 19:37:33 +0200

    network: add further client handle functionality

--- src/network/client_handle.rs
+++ src/network/client_handle.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use super::event_loop::ServerHandle;
+use super::{FromServerToClient, ToServerFromClient};
 use mio::net::{SocketAddr, TcpStream};
 use std::default::Default;
 use themis::keys::EcdsaPublicKey;
@@ -7,27 +9,26 @@ use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinHandle;
 
-pub enum ActorMessage {}
-
 #[derive(Debug)]
 pub struct ClientHandle {
     id: EcdsaPublicKey,
     ip: SocketAddr,
-    chan: Sender<ActorMessage>,
+    chan: Sender<FromServerToClient>,
     kill: JoinHandle<()>,
 }
 
+// A handle used by the server to communicate with clients
 impl ClientHandle {
     pub fn new(
         id: EcdsaPublicKey,
         ip: SocketAddr,
-        chan: Sender<ActorMessage>,
+        chan: Sender<FromServerToClient>,
         kill: JoinHandle<()>,
     ) -> Self {
         Self { id, ip, chan, kill }
     }
 
-    pub fn send(&mut self, msg: ActorMessage) -> Result<(), std::io::Error> {
+    pub fn send(&mut self, msg: FromServerToClient) -> Result<(), std::io::Error> {
         if self.chan.try_send(msg).is_err() {
             Err(std::io::Error::new(
                 std::io::ErrorKind::BrokenPipe,
@@ -47,4 +48,42 @@ impl Drop for ClientHandle {
     fn drop(&mut self) {
         self.kill.abort()
     }
+}
+
+// ClientData contains all data needed to by this actor to communicate with one client
+struct ClientData {
+    id: EcdsaPublicKey,
+    handle: ServerHandle,
+    chan: Receiver<FromServerToClient>,
+    stream: TcpStream,
+}
+
+pub fn spawn_client_actor(
+    id: EcdsaPublicKey,
+    socket: SocketAddr,
+    handle: ServerHandle,
+    stream: TcpStream,
+) {
+    let (send, recv): (Sender<FromServerToClient>, Receiver<FromServerToClient>) =
+        mpsc::channel(32);
+    let cd = ClientData {
+        id,
+        handle: handle.clone(),
+        chan: recv,
+        stream,
+    };
+
+    let (oneshot_tx, oneshot_rx): (
+        oneshot::Sender<ClientHandle>,
+        oneshot::Receiver<ClientHandle>,
+    ) = oneshot::channel();
+    let kill = 1;
+}
+
+async fn client_actor(oneshot_rx: oneshot::Receiver<ClientHandle>, mut data: ClientData) {
+    let oneshot_rx = match oneshot_rx.await {
+        Ok(handle) => handle,
+        Err(_) => return,
+    };
+    data.handle.send(ToServerFromClient::NewClient(oneshot_rx));
 }
