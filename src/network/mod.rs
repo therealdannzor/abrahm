07d4f97f2629914c7ff32c75f0a0a7129f7571ca
dkf <dannzor@outlook.com>
2021-09-02 19:35:16 +0200

    network: move and update client-server io types

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -3,3 +3,12 @@ pub mod common;
 pub mod core;
 pub mod event_loop;
 pub mod message;
+
+// FromServer is the message type sent from the server to clients
+pub struct FromServerToClient(Vec<u8>);
+
+// ToServer is the message type sent from clients to the server
+pub enum ToServerFromClient {
+    Message(themis::keys::EcdsaPublicKey, Vec<u8>),
+    NewClient(crate::network::client_handle::ClientHandle),
+}
