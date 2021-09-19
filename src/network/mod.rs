b635f97b8f050bebbb850739ed1b51c4a34986f8
dkf <dannzor@outlook.com>
2021-09-19 11:41:59 +0200

    network: expand server handlers to include inbound/outbound messages

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,16 +1,18 @@
 pub mod client_handle;
 pub mod common;
 pub mod core;
-pub mod event_loop;
 pub mod message;
+pub mod server_handle;
 pub mod tcp_utils;
 
+#[derive(Clone)]
 // FromServerEvent is the event type emitted from the server
-pub struct FromServerEvent(Vec<u8>);
+pub enum FromServerEvent {
+    Message(mio::Token, Vec<u8>),
+}
 
 // ToServerEvent is the event type the server reacts to (e.g., external messages)
 pub enum ToServerEvent {
-    Message(themis::keys::EcdsaPublicKey, Vec<u8>),
     NewClient(crate::network::client_handle::ClientHandle),
     FatalError(std::io::Error),
 }
--- src/network/server_handle.rs
+++ src/network/server_handle.rs
@@ -1,35 +1,29 @@
 #![allow(unused)]
 
-use super::client_handle::ClientHandle;
 use super::tcp_utils::{handle_client_event, next_token, open_socket};
-use super::ToServerEvent;
+use super::{FromServerEvent, ToServerEvent};
 use mio::net::TcpStream;
-use mio::{Events, Interest, Poll, Registry, Token};
+use mio::{Events, Interest, Poll, Token};
 use std::{
     collections::{HashMap, VecDeque},
-    default::Default,
     io,
-    io::prelude::*,
-    net::{IpAddr, Ipv4Addr, SocketAddr},
-    str::from_utf8,
 };
-use tokio::sync::mpsc::{channel, error::SendError, Receiver, Sender};
-use tokio::sync::oneshot;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::task::JoinHandle;
 
 #[derive(Clone)]
-pub struct ServerHandle {
+pub struct ToServerHandle {
     tx: Sender<ToServerEvent>,
 }
 
-impl ServerHandle {
+impl ToServerHandle {
     pub fn new(tx: Sender<ToServerEvent>) -> Self {
         Self { tx }
     }
 
-    // send is used to signal an alert the server of:
+    // server inbound message of:
     // (1) a new client connection; or
-    // (2) a message from a known client
+    // (2) an error message of a failed upstream
     pub async fn send(&mut self, message: ToServerEvent) {
         if self.tx.send(message).await.is_err() {
             panic!("there is no event loop running!");
@@ -37,11 +31,28 @@ impl ServerHandle {
     }
 }
 
-pub fn spawn_event_listener() -> (ServerHandle, JoinHandle<()>) {
-    let (send, recv): (Sender<ToServerEvent>, Receiver<ToServerEvent>) = channel(32);
-    let handle = ServerHandle::new(send);
+pub struct FromServerHandle {
+    rx: Receiver<FromServerEvent>,
+}
+
+impl FromServerHandle {
+    pub fn new(rx: Receiver<FromServerEvent>) -> Self {
+        Self { rx }
+    }
+
+    // server outbound messages (tuples) of `Token` (id) and `Vec<u8>` (payload)
+    pub async fn receive(&mut self) -> FromServerEvent {
+        self.rx.recv().await.unwrap()
+    }
+}
+
+pub fn spawn_event_listener() -> (ToServerHandle, FromServerHandle, JoinHandle<()>) {
+    let (send_ts, recv_ts): (Sender<ToServerEvent>, Receiver<ToServerEvent>) = channel(32);
+    let (send_fs, recv_fs): (Sender<FromServerEvent>, Receiver<FromServerEvent>) = channel(32);
+    let ts_handle = ToServerHandle::new(send_ts);
+    let fs_handle = FromServerHandle::new(recv_fs);
     let join = tokio::spawn(async move {
-        let res = match event_loop(recv).await {
+        let res = match event_loop(recv_ts, send_fs).await {
             Ok(()) => {}
             Err(e) => {
                 panic!("event loop failed: {:?}", e);
@@ -49,26 +60,28 @@ pub fn spawn_event_listener() -> (ServerHandle, JoinHandle<()>) {
         };
     });
 
-    (handle, join)
+    (ts_handle, fs_handle, join)
 }
 
-const PEER_TOKEN: Token = Token(0);
-const SERVER_TOKEN: Token = Token(1024);
 const ECDSA_PUB_KEY_SIZE_BITS: usize = 90;
 const MESSAGE_SIZE: usize = 256; // TODO: assert proper size
+async fn event_loop(
+    recv: Receiver<ToServerEvent>,
+    tx: Sender<FromServerEvent>,
+) -> Result<(), io::Error> {
+    const PEER_TOKEN: Token = Token(0);
+    const SERVER_TOKEN: Token = Token(1024);
 
-async fn event_loop(mut recv: Receiver<ToServerEvent>) -> Result<(), io::Error> {
     let mut unique_token = Token(PEER_TOKEN.0 + 1);
     let mut connections: HashMap<Token, TcpStream> = HashMap::new();
     let mut poller = Poll::new()?;
     let mut events = Events::with_capacity(1024);
-    let mut client_handle: HashMap<Token, ClientHandle> = HashMap::new();
     let mut mailbox: HashMap<Token, VecDeque<String>> = HashMap::new();
 
     // create listening server and register it will poll to receive events
     let mut server = open_socket();
     let port = server.local_addr().unwrap().port();
-    poller
+    let _ = poller
         .registry()
         .register(&mut server, SERVER_TOKEN, Interest::READABLE);
 
@@ -78,8 +91,8 @@ async fn event_loop(mut recv: Receiver<ToServerEvent>) -> Result<(), io::Error> 
         for event in events.iter() {
             match event.token() {
                 SERVER_TOKEN => loop {
-                    let (mut conn, address) = match server.accept() {
-                        Ok((conn, address)) => (conn, address),
+                    let (mut conn, _address) = match server.accept() {
+                        Ok((conn, _address)) => (conn, _address),
                         Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
                             // this is normal, try again
                             break;
@@ -91,15 +104,19 @@ async fn event_loop(mut recv: Receiver<ToServerEvent>) -> Result<(), io::Error> 
                     };
 
                     let new_friend_token = next_token(&mut unique_token);
-                    poller
-                        .registry()
-                        .register(&mut conn, new_friend_token, Interest::WRITABLE);
+                    let _ =
+                        poller
+                            .registry()
+                            .register(&mut conn, new_friend_token, Interest::WRITABLE);
                     connections.insert(new_friend_token, conn);
                 },
                 client_token => loop {
                     // maybe we have an event
-                    let done = if let Some(conn) = connections.get_mut(&client_token) {
+                    let _done = if let Some(conn) = connections.get_mut(&client_token) {
                         let resp = handle_client_event(conn, event, None);
+                        if resp.is_ok() {
+                            tx.send(FromServerEvent::Message(client_token, resp.unwrap()));
+                        }
                         true
                     } else {
                         // false alarm, no event, just ignore
