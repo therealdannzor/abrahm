5e4e350c963d414a7112e1239969783949ebaf99
dkf <dannzor@outlook.com>
2021-09-25 11:11:33 +0200

    network: complete rewrite of client handle to communicate with other peers

--- src/network/client_handle.rs
+++ src/network/client_handle.rs
@@ -1,132 +1,61 @@
 #![allow(unused)]
 
-use super::server_handle::ToServerHandle;
-use super::{FromServerEvent, ToServerEvent};
-use mio::net::{TcpListener, TcpStream};
-use mio::{Events, Interest, Poll, Token};
-use std::default::Default;
-use std::io;
+use super::{DialEvent, FromServerEvent, InternalMessage};
+use mio::net::UdpSocket;
+use mio::Token;
+use std::collections::HashMap;
 use std::net::SocketAddr;
-use themis::keys::EcdsaPublicKey;
-use tokio::sync::mpsc::{Receiver, Sender};
-use tokio::sync::{mpsc, oneshot};
-use tokio::task::JoinHandle;
+use tokio::sync::mpsc::{Receiver, Sender, UnboundedSender};
 
-#[derive(Debug)]
-pub struct ClientHandle {
-    id: Token,
-    socket: SocketAddr,
-    chan: Sender<FromServerEvent>,
-    kill: JoinHandle<()>,
+struct ToPeer {
+    server: UdpSocket,
+    dest: UdpSocket,
 }
 
-// A handle used by the server to communicate with the client
-impl ClientHandle {
-    pub fn new(
-        id: Token,
-        socket: SocketAddr,
-        chan: Sender<FromServerEvent>,
-        kill: JoinHandle<()>,
-    ) -> Self {
-        Self {
-            id,
-            socket,
-            chan,
-            kill,
+// peer_loop contains the operations that concern all the peers connected to the server.
+async fn peer_loop(mut rx: Receiver<InternalMessage>, mut internal_tx: Sender<InternalMessage>) {
+    let mut id_conns: HashMap<Token, SocketAddr> = HashMap::new();
+    let mut stream_conns: HashMap<SocketAddr, UdpSocket> = HashMap::new();
+
+    while let Some(msg) = rx.recv().await {
+        match msg {
+            InternalMessage::FromServerEvent(FromServerEvent::HostSocket(addr)) => {
+                let sock = UdpSocket::bind(addr).unwrap();
+                stream_conns.insert(addr, sock);
+            }
+            InternalMessage::FromServerEvent(FromServerEvent::NewClient(msg)) => {
+                let op = id_conns.insert(msg.1, msg.2);
+                if op.is_some() {
+                    log::warn!(
+                        "updated socket address for token {:?}, old={:?}, new={:?}",
+                        msg.1,
+                        op.unwrap(),
+                        msg.2
+                    );
+                }
+            }
+            InternalMessage::DialEvent(DialEvent::Message {
+                send_to,
+                payload,
+                response,
+            }) => {
+                let recipient = id_conns.get(&send_to);
+                if recipient.is_none() {
+                    log::warn!("trying to send message to unknown peer: {:?}", recipient);
+                    continue;
+                }
+                let recipient = recipient.unwrap();
+                let host_sock_addr = id_conns.get(&Token(1024)).unwrap();
+                let host_sock = stream_conns.get(host_sock_addr).unwrap();
+                host_sock.connect(*recipient);
+                let attempt_send = host_sock.send(&payload);
+                if attempt_send.is_err() {
+                    response.send(0);
+                } else {
+                    let bytes_sent = attempt_send.unwrap();
+                    response.send(bytes_sent);
+                }
+            }
         }
     }
-
-    // send a message to this client actor
-    pub fn send(&mut self, msg: FromServerEvent) -> Result<(), std::io::Error> {
-        if self.chan.try_send(msg).is_err() {
-            Err(std::io::Error::new(
-                std::io::ErrorKind::BrokenPipe,
-                "channel disconnected or full buffer",
-            ))
-        } else {
-            Ok(())
-        }
-    }
-
-    pub fn kill(self) {
-        drop(self);
-    }
-}
-
-impl Drop for ClientHandle {
-    fn drop(&mut self) {
-        self.kill.abort()
-    }
-}
-
-// ClientData contains internal data needed to by this actor to communicate with a client
-struct ClientData {
-    id: Token,
-    handle: ToServerHandle,
-    chan: Receiver<FromServerEvent>,
-    stream: TcpStream,
-}
-
-pub async fn open_client_factory(id: Token, socket: SocketAddr, mut handle: ToServerHandle) {
-    match client_factory_loop(id, socket, handle.clone()) {
-        Ok(()) => {}
-        Err(e) => {
-            handle.send(ToServerEvent::FatalError(e)).await;
-        }
-    }
-}
-
-fn client_factory_loop(
-    id: Token,
-    socket: SocketAddr,
-    handle: ToServerHandle,
-) -> Result<(), io::Error> {
-    let listen = TcpListener::bind(socket)?;
-
-    loop {
-        let (tcp_connection, socket_addr) = listen.accept()?;
-        spawn_client_actor(id, socket_addr, handle.clone(), tcp_connection);
-    }
-}
-
-fn spawn_client_actor(id: Token, socket: SocketAddr, handle: ToServerHandle, stream: TcpStream) {
-    let (mpsc_tx, mpsc_rx): (Sender<FromServerEvent>, Receiver<FromServerEvent>) =
-        mpsc::channel(32);
-    let cd = ClientData {
-        id: id.clone(),
-        handle: handle.clone(),
-        chan: mpsc_rx,
-        stream,
-    };
-
-    let (oneshot_tx, oneshot_rx): (
-        oneshot::Sender<ClientHandle>,
-        oneshot::Receiver<ClientHandle>,
-    ) = oneshot::channel();
-    let kill = tokio::spawn(client_actor(oneshot_rx, cd));
-
-    let handle = ClientHandle::new(id, socket, mpsc_tx, kill);
-
-    let _ = oneshot_tx.send(handle);
-}
-
-async fn client_actor(oneshot_rx: oneshot::Receiver<ClientHandle>, mut data: ClientData) {
-    let oneshot_rx = match oneshot_rx.await {
-        Ok(handle) => handle,
-        Err(_) => return,
-    };
-
-    // send a receiver part to the server to receive events from the client
-    data.handle.send(ToServerEvent::NewClient(oneshot_rx)).await;
-
-    match client_worker(data).await {
-        Ok(()) => {}
-        Err(e) => eprintln!("this should not happen: {}", e),
-    }
-}
-
-async fn client_worker(mut data: ClientData) -> Result<(), std::io::Error> {
-    let mut poller = Poll::new()?;
-    let mut events = Events::with_capacity(256);
-    Ok(())
 }
