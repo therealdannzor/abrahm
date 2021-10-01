0993b67c5ce75da2a3575b47ce00a3cbe18faea9
dkf <dannzor@outlook.com>
2021-10-01 14:25:35 +0200

    chore: remove unused p2p module

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod blockchain;
 mod consensus;
 mod ledger;
 mod network;
-mod p2p;
 mod swiss_knife;
 mod types;
 fn main() {}
--- src/p2p/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod net_behaviour;
-pub mod proto_handler;
-pub mod proto_upgrade;
-pub mod ws_handler;
-pub mod ws_peer;
-pub mod ws_routes;
