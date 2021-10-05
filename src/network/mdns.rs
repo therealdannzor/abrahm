0cf8ac63af0ccb8db916d10dcb4ae55a91a61c68
dkf <dannzor@outlook.com>
2021-10-05 17:33:16 +0200

    network: rollback of floodsub to use regular udp

--- src/network/mdns.rs
+++ src/network/mdns.rs
@@ -2,149 +2,48 @@
 
 use crate::hashed;
 use crate::swiss_knife::helper::{generate_hash_from_input, sign_message_digest};
-use async_std::{io, task};
 use futures::StreamExt;
-use futures::{future, prelude::*};
 use libp2p::{
-    floodsub::{self, Floodsub, FloodsubEvent},
     identity,
     mdns::{Mdns, MdnsConfig, MdnsEvent},
-    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmEvent},
-    NetworkBehaviour, PeerId,
+    swarm::{Swarm, SwarmEvent},
+    PeerId,
 };
 use std::error::Error;
 use std::net::SocketAddr;
-use std::task::{Context, Poll};
 use themis::keys::{EcdsaPrivateKey, EcdsaPublicKey};
 use tokio::net::UdpSocket;
 
-pub async fn peer_discovery_loop(amount_to_find: usize) -> Result<(), Box<dyn Error>> {
+pub async fn peer_discovery_loop(
+    pk: EcdsaPublicKey,
+    sk: EcdsaPrivateKey,
+    server_port: String,
+) -> Result<(), Box<dyn Error>> {
     let keys_id = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from(keys_id.public());
     let transport = libp2p::development_transport(keys_id).await?;
-    let floodsub_topic = floodsub::Topic::new("discovery");
+    let behaviour = Mdns::new(MdnsConfig::default()).await?;
 
-    #[derive(NetworkBehaviour)]
-    #[behaviour(out_event = "OutEvent")]
-    struct MyBehaviour {
-        floodsub: Floodsub,
-        mdns: Mdns,
-
-        #[behaviour(ignore)]
-        #[allow(dead_code)]
-        ignored_member: bool,
-    }
-
-    #[derive(Debug)]
-    enum OutEvent {
-        Floodsub(FloodsubEvent),
-        Mdns(MdnsEvent),
-    }
-
-    impl From<MdnsEvent> for OutEvent {
-        fn from(v: MdnsEvent) -> Self {
-            Self::Mdns(v)
-        }
-    }
-
-    impl From<FloodsubEvent> for OutEvent {
-        fn from(v: FloodsubEvent) -> Self {
-            Self::Floodsub(v)
-        }
-    }
-
-    impl NetworkBehaviourEventProcess<FloodsubEvent> for MyBehaviour {
-        fn inject_event(&mut self, message: FloodsubEvent) {
-            if let FloodsubEvent::Message(msg) = message {
-                log::info!(
-                    "Received: {:?} from {:?}",
-                    String::from_utf8_lossy(&msg.data),
-                    msg.source
-                );
-            }
-        }
-    }
-
-    impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
-        fn inject_event(&mut self, event: MdnsEvent) {
-            match event {
-                MdnsEvent::Discovered(list) => {
-                    for (peer, _) in list {
-                        self.floodsub.add_node_to_partial_view(peer);
-                    }
-                }
-                MdnsEvent::Expired(list) => {
-                    for (peer, _) in list {
-                        if !self.mdns.has_node(&peer) {
-                            self.floodsub.remove_node_from_partial_view(&peer);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    let mut swarm = {
-        let mdns = task::block_on(Mdns::new(MdnsConfig::default()))?;
-        let mut behaviour = MyBehaviour {
-            floodsub: Floodsub::new(peer_id),
-            mdns,
-            ignored_member: false,
-        };
-
-        behaviour.floodsub.subscribe(floodsub_topic.clone());
-        Swarm::new(transport, behaviour, peer_id)
-    };
-
+    let mut swarm = Swarm::new(transport, behaviour, peer_id);
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    let mut validator_disc_addresses: Vec<String> = Vec::new();
-
-    // search the local network for other peers and bail out when having found 'em all
-    task::block_on(future::poll_fn(move |cx: &mut Context<'_>| {
-        loop {
-            match swarm.poll_next_unpin(cx) {
-                Poll::Ready(Some(SwarmEvent::NewListenAddr { address, .. })) => {
-                    log::info!("Listening on {:?}", address);
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::Behaviour(MdnsEvent::Discovered(peers)) => {
+                for (_, addr) in peers {
+                    println!("Peer address: {:?}", addr);
+                    let port = extract_port(addr);
+                    println!("Discovery at: {}", port);
                 }
-                Poll::Ready(Some(SwarmEvent::Behaviour(OutEvent::Floodsub(
-                    FloodsubEvent::Message(msg),
-                )))) => {
-                    log::info!(
-                        "Received: {:?} from {:?}",
-                        String::from_utf8_lossy(&msg.data),
-                        msg.source
-                    );
-                }
-                Poll::Ready(Some(SwarmEvent::Behaviour(OutEvent::Mdns(
-                    MdnsEvent::Discovered(list),
-                )))) => {
-                    for (peer, _) in list {
-                        swarm
-                            .behaviour_mut()
-                            .floodsub
-                            .add_node_to_partial_view(peer);
-                    }
-                }
-                Poll::Ready(Some(_)) => {}
-                Poll::Ready(None) => {}
-                Poll::Pending => break,
             }
+            _ => {}
         }
-        Poll::Pending
-    }))
+    }
 }
 
 fn extract_port(multi_address: libp2p::Multiaddr) -> String {
-    let addr_as_u8_vec = &multi_address.to_vec();
-    let conv = match std::str::from_utf8(addr_as_u8_vec) {
-        Ok(s) => s.to_string(),
-        Err(e) => {
-            panic!("this should not happen: {:?}", e);
-        }
-    };
-
-    let sep: Vec<&str> = conv.split("/").collect();
+    let multistr = multi_address.to_string();
+    let sep: Vec<&str> = multistr.split("/").collect();
     let last_ind = sep.len() - 1;
     let port = sep[last_ind];
     port.to_string()
@@ -163,18 +62,10 @@ fn create_discv_handshake(
     secret_key: EcdsaPrivateKey,
     port_string: String,
 ) -> Vec<u8> {
-    // store the message here
-    let mut message: Vec<u8> = Vec::new();
-
-    let k = public_key.as_ref().to_vec();
-    let mut k = match std::str::from_utf8(&k) {
-        Ok(key) => key,
-        Err(e) => panic!("this should not happen (upstream error key): {:?}", e),
-    };
-    k.to_string().push_str(&port_string);
-    message.extend(k.as_bytes().to_vec()); // adds public key + port to the buffer
-    let key_and_port_hashed = hashed!(k);
+    let mut k = public_key.as_ref().to_vec();
+    k.extend(port_string.as_bytes().to_vec());
+    let key_and_port_hashed = hashed!(&String::from_utf8_lossy(&k));
     let signed = sign_message_digest(secret_key, key_and_port_hashed.as_ref());
-    message.extend(signed); // add the signed hash to the buffer
-    message
+    k.extend(signed); // add the signed hash to the buffer
+    k
 }
