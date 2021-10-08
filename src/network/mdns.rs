a325c52363acf67d9fcf3975bb8b92e60ba00485
dkf <dannzor@outlook.com>
2021-10-08 14:48:20 +0200

    network: add ability to correctly verify discv handshakes

--- src/network/mdns.rs
+++ src/network/mdns.rs
@@ -2,7 +2,8 @@
 
 use super::common::cmp_message_with_signed_digest;
 use crate::hashed;
-use crate::swiss_knife::helper::{hash_from_vec_u8_input, sign_message_digest};
+use crate::network::common::public_key_and_port_to_vec;
+use crate::swiss_knife::helper::hash_and_sign_message_digest;
 use futures::StreamExt;
 use libp2p::{
     identity,
@@ -48,7 +49,6 @@ pub async fn peer_discovery_loop(
                             let _ = socket
                                 .send_to(&broadcast_disc_msg, recipient_addr.clone())
                                 .await;
-                            println!("Dispatched message to {}", recipient_addr);
                         }
                     });
                 }
@@ -98,16 +98,9 @@ impl Server {
             let two_sec = std::time::Duration::from_secs(2);
             std::thread::sleep(two_sec);
             if let Some(size) = to_send {
-                println!("Size of message: {}", buf.len());
-                println!(
-                    "Is valid handshake message: {}",
-                    verify_discv_handshake(buf.clone())
-                );
+                log::info!("valid handshake? {}", verify_discv_handshake(buf.clone()));
+                buf = vec![0; 198];
             }
-            println!(
-                "Nothing to send, receiving messages at {}",
-                socket.local_addr().unwrap()
-            );
             to_send = Some(socket.recv(&mut buf).await?);
         }
     }
@@ -138,34 +131,21 @@ fn create_discv_handshake(
     port: String,
 ) -> Vec<u8> {
     // First half is PUBLIC_KEY | PORT (in bytes)
-    let mut first_half = public_key_and_port_to_vec(public_key, port);
+    let mut result = public_key_and_port_to_vec(public_key, port);
 
     // Second half is H(PUBLIC_KEY | PORT)_C (in bytes)
-    let second_half = hash_and_sign(first_half.clone(), secret_key);
+    let second_half = hash_and_sign_message_digest(secret_key, result.clone());
 
-    let mut result = Vec::new();
-    result.extend(&first_half);
     result.extend(second_half);
-
     result
 }
 
-fn public_key_and_port_to_vec(key: EcdsaPublicKey, port: String) -> Vec<u8> {
-    let mut res = key.as_ref().to_vec(); // this contains non ASCII characters
-    res.extend(port.as_bytes().to_vec());
-    res
-}
-
-fn hash_and_sign(input: Vec<u8>, secret_key: EcdsaPrivateKey) -> Vec<u8> {
-    let hashed = hash_from_vec_u8_input(input.clone());
-    let signed_hash = sign_message_digest(secret_key, hashed.as_ref());
-    signed_hash
-}
-
 fn verify_discv_handshake(message: Vec<u8>) -> bool {
+    let full_length = message.len();
     // messages are between length 196 and 198
-    if message.len() > 198 {
-        panic!("incorrect size of handshake message: {}", message.len());
+    if full_length > 198 || full_length < 196 {
+        log::error!("message length not between 196 and 198");
+        return false;
     }
     const PUB_KEY_LEN: usize = 45;
     const SRV_PORT_LEN: usize = 5;
@@ -174,12 +154,30 @@ fn verify_discv_handshake(message: Vec<u8>) -> bool {
     let mut plain_message = Vec::new();
     plain_message.extend_from_slice(public_key);
     plain_message.extend_from_slice(port);
-    let signed_message = message[PUB_KEY_LEN + SRV_PORT_LEN..].to_vec();
+
+    // a hack to make sure that the signed message does not include
+    // zeros that the peer never intended to be part of the message
+    let last_three_elements = message[full_length - 3..].to_vec();
+    let trailing_zeros = check_zeros(last_three_elements);
+    let signed_message = message[PUB_KEY_LEN + SRV_PORT_LEN..full_length - trailing_zeros].to_vec();
 
     let public_key = match EcdsaPublicKey::try_from_slice(public_key) {
         Ok(k) => k,
-        Err(_) => return false,
+        Err(_) => {
+            log::error!("could not restore public key from slice");
+            return false;
+        }
     };
 
     cmp_message_with_signed_digest(public_key, plain_message, signed_message)
+}
+
+fn check_zeros(v: Vec<u8>) -> usize {
+    let mut result = 0;
+    for num in v.iter() {
+        if *num == 0 {
+            result += 1;
+        }
+    }
+    result
 }
