e552b56cc66faa63607b917a5768a31f062c0793
dkf <dannzor@outlook.com>
2021-10-29 18:01:09 +0200

    ledger: fix bug where local keypair was not loaded properly

--- src/ledger/keystore.rs
+++ src/ledger/keystore.rs
@@ -39,37 +39,18 @@ pub struct KeyPairAsType {
 
 impl KeyStore {
     pub fn new(index: u32) -> Self {
-        // dummy initializes
-        let mut local_pub_key = "".to_string();
-        let mut local_sec_key = "".to_string();
-        let (mut local_sec_type, mut local_pub_type) = gen_ec_key_pair().split();
-        let mut local_key_path = "".to_string();
-
-        let mut path: String = env!("CARGO_MANIFEST_DIR", "missing cargo manifest").to_string();
-        path.push_str("/keys/node");
-
         for i in 0..4 {
-            let mut copy_path = path.clone();
-            let digit = std::char::from_digit(i, 10).unwrap();
-            copy_path.push(digit);
-            let _ = match std::fs::metadata(copy_path.clone()) {
+            let mut path = create_node_key_path(i);
+            let _ = match std::fs::metadata(path.clone()) {
                 Ok(_) => continue,
                 Err(_) => {
-                    let _ = std::fs::create_dir_all(&copy_path);
+                    let _ = std::fs::create_dir_all(&path);
                     // generate public-private key pair
                     let (secret_key, public_key) = gen_ec_key_pair().split();
 
-                    // convert public key to hexadecimal string as a more readable ID
+                    // convert public key to hexadecimal string to be more readable
                     let public_hex: String = hex::encode(public_key.clone());
                     let secret_hex: String = hex::encode(secret_key.clone());
-                    // based on cli arg, we set one of the keypairs as the local node/peers ID
-                    if i == index {
-                        local_pub_key = public_hex.clone();
-                        local_sec_key = secret_hex.clone();
-                        local_pub_type = public_key.clone();
-                        local_sec_type = secret_key.clone();
-                        local_key_path = copy_path.clone();
-                    }
 
                     let key_pair_hex = KeyFile::new(public_hex, secret_hex);
                     let key_json = match serde_json::to_string(&key_pair_hex) {
@@ -79,20 +60,57 @@ impl KeyStore {
                         }
                     };
 
-                    copy_path.push_str("/keyfile.dat");
-                    match helper::write_file(&key_json, &copy_path) {
+                    path.push_str("/keyfile.dat");
+                    match helper::write_file(&key_json, &path) {
                         Ok(_) => (),
                         Err(e) => {
-                            eprintln! {"failed to create secret key at: {}, error: {}", copy_path, e}
+                            eprintln! {"failed to create secret key at: {}, error: {}", path, e}
                         }
                     }
                 }
             };
         }
 
+        // based on cli arg, we set one of the keypairs as the local node / peer ID
+        let local_node_dir = create_node_key_path(index);
+        let (sk_hex, pk_hex) = read_keyfile_json(local_node_dir.clone());
+        let mut sk_bytes = sk_hex.clone().into_bytes();
+        // remove quotation marks in the beginning and the end of the byte vector
+        sk_bytes.remove(0);
+        sk_bytes.remove(sk_bytes.len() - 1);
+        let sk_vec = match hex::decode(sk_bytes) {
+            Ok(s) => s,
+            Err(e) => {
+                panic!("failed to decode secret key hex string: {}", e);
+            }
+        };
+        let local_sec_type = match EcdsaPrivateKey::try_from_slice(sk_vec) {
+            Ok(s) => s,
+            Err(e) => {
+                panic!("failed to re-create secret key from string: {}", e);
+            }
+        };
+
+        let mut pk_bytes = pk_hex.clone().into_bytes();
+        // remove quotation marks in the beginning and the end of the byte vector
+        pk_bytes.remove(0);
+        pk_bytes.remove(pk_bytes.len() - 1);
+        let pk_vec = match hex::decode(pk_bytes) {
+            Ok(s) => s,
+            Err(e) => {
+                panic!("failed to decode public key hex string: {}", e);
+            }
+        };
+        let local_pub_type = match EcdsaPublicKey::try_from_slice(pk_vec) {
+            Ok(s) => s,
+            Err(e) => {
+                panic!("failed to re-create public key from string: {}", e);
+            }
+        };
+
         let key_pair_hex = KeyFile {
-            public_hex: local_pub_key,
-            secret_hex: local_sec_key,
+            public_hex: pk_hex.clone(),
+            secret_hex: sk_hex.clone(),
         };
         let key_pair_as_type = KeyPairAsType {
             public: local_pub_type,
@@ -102,7 +120,7 @@ impl KeyStore {
         Self {
             key_pair_hex,
             key_pair_as_type,
-            key_path: local_key_path,
+            key_path: local_node_dir,
             tx_count: 0,
         }
     }
@@ -118,4 +136,24 @@ impl KeyStore {
     pub fn get_secret_as_type(&self) -> EcdsaPrivateKey {
         self.key_pair_as_type.secret.clone()
     }
+}
+
+fn read_keyfile_json(mut path: String) -> (String, String) {
+    path.push_str("/keyfile.dat");
+    let json_fp = std::path::Path::new(&path);
+    let file = std::fs::File::open(json_fp).expect("file not found");
+    let keypair: serde_json::Value = serde_json::from_reader(file).expect("could not read file");
+
+    let sk = keypair["secret_hex"].to_string();
+    let pk = keypair["public_hex"].to_string();
+
+    (sk, pk)
+}
+
+fn create_node_key_path(id: u32) -> String {
+    let mut path: String = env!("CARGO_MANIFEST_DIR", "missing cargo manifest").to_string();
+    path.push_str("/keys/node");
+    let digit = std::char::from_digit(id, 10).unwrap();
+    path.push(digit);
+    path
 }
