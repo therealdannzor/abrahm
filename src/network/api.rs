fa5996e6c183ced2a07fd6dce2802b51d12adc1c
dkf <dannzor@outlook.com>
2021-11-01 19:38:36 +0100

    network: return vec of all found peers in discv frontend api

--- src/network/api.rs
+++ src/network/api.rs
@@ -48,23 +48,36 @@ pub struct NetworkIoHandlers {
 
 // spawn_peer_discovery_listener finds the other peers on the same network.
 // It needs to know the host server loop to tell other peers to speak with it there.
-// This port is received through calling `spawn_io_listeners` successfully.
+// This port is received by a succcessful call to `spawn_io_listeners`.
+// When it has discovered all peers, it returns all [port, public_key] pairs.
 pub async fn spawn_peer_discovery_listener(
     pk: EcdsaPublicKey,
     sk: EcdsaPrivateKey,
     server_port: String,
     validator_list: Vec<String>,
-) -> Receiver<ValidatedPeer> {
-    let (tx_peer_discv, rx_peer_discv): (Sender<ValidatedPeer>, Receiver<ValidatedPeer>) =
+) -> Vec<ValidatedPeer> {
+    let (tx_peer_discv, mut rx_peer_discv): (Sender<ValidatedPeer>, Receiver<ValidatedPeer>) =
         mpsc::channel(8);
 
+    // subtract by one because we have already discovered ourself, by definition
+    let amount_to_validate = validator_list.len() - 1;
+
     let join = tokio::spawn(async move {
-        spawn_peer_discovery_loop(pk, sk, server_port, tx_peer_discv, validator_list).await
+        spawn_peer_discovery_loop(pk, sk, server_port, tx_peer_discv, validator_list.clone()).await
     });
 
     join.await;
 
-    rx_peer_discv
+    let mut peers_found: Vec<ValidatedPeer> = Vec::new();
+    while let Some(msg) = rx_peer_discv.recv().await {
+        peers_found.push(msg);
+        if peers_found.len() == amount_to_validate {
+            break;
+        }
+    }
+
+    log::info!("returning a list of the discovered peers");
+    peers_found
 }
 
 pub async fn spawn_io_listeners(val_set: Vec<String>) -> String {
