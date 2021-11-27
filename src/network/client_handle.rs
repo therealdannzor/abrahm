05dca8a1ea04d763aa41d897a3eb23354607d187
dkf <dannzor@outlook.com>
2021-11-27 17:56:16 +0100

    network: implement response from client handle when server sends new client data

--- src/network/client_handle.rs
+++ src/network/client_handle.rs
@@ -1,9 +1,13 @@
-use super::{DialEvent, FromServerEvent, InternalMessage, OrdPayload, PayloadEvent};
+use super::common::{create_p2p_message, extract_server_port_field};
+use super::{
+    DialEvent, FromServerEvent, InternalMessage, OrdPayload, PayloadEvent, UpgradedPeerData,
+};
 use mio::net::UdpSocket;
 use mio::Token;
 use std::collections::HashMap;
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::Arc;
+use themis::keys::{EcdsaPrivateKey, EcdsaPublicKey};
 use tokio::sync::Notify;
 use tokio::sync::{
     mpsc::{Receiver, Sender},
@@ -15,13 +19,20 @@ pub struct MessagePeerHandle(
     Sender<InternalMessage>,
     // transmit channel to request messages received from other peers
     Sender<PayloadEvent>,
+    // receive upgraded peer data
+    Receiver<UpgradedPeerData>,
 );
 
 impl MessagePeerHandle {
-    pub fn new(tx_outbound: Sender<InternalMessage>, tx_inbound: Sender<PayloadEvent>) -> Self {
+    pub fn new(
+        tx_outbound: Sender<InternalMessage>,
+        tx_inbound: Sender<PayloadEvent>,
+        rx_upgrade: Receiver<UpgradedPeerData>,
+    ) -> Self {
         Self {
             0: tx_outbound,
             1: tx_inbound,
+            2: rx_upgrade,
         }
     }
 
@@ -66,12 +77,14 @@ impl MessagePeerHandle {
 }
 
 pub async fn spawn_peer_listeners(
+    pk: EcdsaPublicKey,
+    sk: EcdsaPrivateKey,
     rx: Receiver<InternalMessage>,
     rx2: Receiver<PayloadEvent>,
     notify: Arc<Notify>,
 ) {
     let join_peer = tokio::spawn(async move {
-        peer_loop(rx, notify).await;
+        peer_loop(pk, sk, rx, notify).await;
     });
     let join_upgraded = tokio::spawn(async move {
         peer_upgraded_loop(rx2).await;
@@ -109,13 +122,22 @@ async fn peer_upgraded_loop(mut rx: Receiver<PayloadEvent>) {
 }
 
 // peer_loop supports the internal operations of the peers it is connected to
-async fn peer_loop(mut rx: Receiver<InternalMessage>, notify: Arc<Notify>) {
+async fn peer_loop(
+    pk: EcdsaPublicKey,
+    sk: EcdsaPrivateKey,
+    mut rx: Receiver<InternalMessage>,
+    notify: Arc<Notify>,
+) {
+    let hex_key_to_token: HashMap<String, Token> = HashMap::new();
+    let hex_key_to_token = Arc::new(Mutex::new(hex_key_to_token));
     let token_to_sock: HashMap<Token, SocketAddr> = HashMap::new();
     let token_to_sock = Arc::new(Mutex::new(token_to_sock));
     let server_token = Token(1024);
     let mut server_udp: Option<Arc<Mutex<UdpSocket>>> = None;
     loop {
         while let Some(msg) = rx.recv().await {
+            let secret_key = sk.clone();
+            let public_key = pk.clone();
             match msg {
                 InternalMessage::FromServerEvent(FromServerEvent::HostSocket(addr, sock)) => {
                     let arc = token_to_sock.clone();
@@ -142,18 +164,71 @@ async fn peer_loop(mut rx: Receiver<InternalMessage>, notify: Arc<Notify>) {
                     drop(idc);
                 }
                 InternalMessage::FromServerEvent(FromServerEvent::NewClient(msg)) => {
-                    log::debug!("client handle registered a new client: {}", msg.2);
+                    log::debug!(
+                        "client handle registered a new client event, id: {}",
+                        msg.1 .0
+                    );
+                    let addr = "127.0.0.1:0".parse().unwrap();
+                    let socket = match UdpSocket::bind(addr) {
+                        Ok(sok) => sok,
+                        Err(e) => {
+                            panic!("udp socket error respond to new client event: {}", e);
+                        }
+                    };
+
+                    // Create a message that tells the peer, who recently connected, that
+                    // we have accepted your connection, given you a new short id (Token),
+                    // and that you will be able to send messages without having to write
+                    // your public key. Instead, use a token id instead, and speak on a
+                    // dedicated port. This port is only designated for communication from
+                    // you to me (unidirectional).
+                    let mut payload = "ACK=".to_string();
+                    // This is what I know you as when you want to speak with me. This way,
+                    // you don't have to send me your whole public key as a hex string
+                    // every time you want to ping me. Remember, every time you speak with
+                    // me from now on, I *only* recognize you by this short id.
+                    let peer_id: String = msg.1.clone().0.to_string();
+                    // This is the port that is exclusively for you from now on. So if you
+                    // use your short id and send messages to me on this socket, I will be
+                    // able to verify the authenticity of your messages and know it is you.
+                    let port = msg.2.port().to_string();
+
+                    payload.push_str(&peer_id);
+                    payload.push_str(&port);
+
+                    // create p2p message that includes the public key as hex string as a
+                    // final confirmation of who the recipient is speaking to and who now
+                    // have given the recipient a short code, i.e. upgraded the connection
+                    // between the two
+                    let full_msg = create_p2p_message(public_key, secret_key, &payload);
+
+                    let mut resp_address = "127.0.0.1:".to_string();
+                    let response_port = msg.3.parse::<u16>().unwrap(); // already sanitized
+                    let respond_to_socket =
+                        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), response_port);
+
+                    if let Err(e) = socket.send_to(&full_msg, respond_to_socket) {
+                        log::error!("failed to send ack response to peer: {}", e);
+                    };
+                    log::info!(
+                        "client handle responded to new client with short id: {}",
+                        msg.1 .0
+                    );
+
                     let arc = token_to_sock.clone();
                     let mut idc = arc.lock().await;
                     let op = idc.insert(msg.1, msg.2);
                     if op.is_some() {
                         log::warn!(
-                            "updated socket address for token {:?}, old={:?}, new={:?}",
+                            "client handle updated socket address for token {:?}, old={:?}, new={:?}",
                             msg.1,
                             op.unwrap(),
                             msg.2
                         );
                     }
+                    let arc = hex_key_to_token.clone();
+                    let mut idc = arc.lock().await;
+                    idc.insert(msg.0, msg.1);
                 }
                 InternalMessage::DialEvent(DialEvent::DispatchMessage(
                     send_to,
