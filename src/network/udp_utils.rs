2f1a0ce11f38ad63dc4aabe24ee8d9714e1e559d
dkf <dannzor@outlook.com>
2021-12-02 10:50:51 +0100

    network: replace mio udp server with tokio udp server as main backend

--- src/network/udp_utils.rs
+++ src/network/udp_utils.rs
@@ -1,43 +1,22 @@
-use mio::net::UdpSocket;
-use mio::{Interest, Poll, Token};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::{Arc, Mutex};
+use tokio::net::UdpSocket;
 
-pub fn net_open() -> (Poll, Arc<Mutex<UdpSocket>>, SocketAddr) {
-    let addr = "127.0.0.1:0".parse().unwrap();
-    let mut socket = match UdpSocket::bind(addr) {
+pub async fn get_udp_and_addr() -> (UdpSocket, String) {
+    let mut socket = match UdpSocket::bind("127.0.0.1:0").await {
         Ok(s) => s,
         Err(e) => panic!("could not bind socket, error: {:?}", e),
     };
 
-    const SOCKET_TOK: Token = Token(1024); // token which represents the server
-    const INTERESTS: Interest = Interest::READABLE.add(Interest::WRITABLE);
+    let port = socket.local_addr().unwrap().port().to_string();
 
-    let poller = Poll::new().unwrap();
-    let _ = poller
-        .registry()
-        .register(&mut socket, SOCKET_TOK, INTERESTS);
-
-    let port = socket.local_addr().unwrap().port();
-    let sock_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
-
-    (poller, Arc::new(Mutex::new(socket)), sock_addr)
+    (socket, port)
 }
 
-// Updates the token to make sure we have unique ones for each stream.
-pub fn next_token(current: &mut Token) -> Token {
-    let next = current.0;
-    current.0 += 1;
-    Token(next)
-}
-
-pub fn any_udp_socket() -> UdpSocket {
-    let addr = "127.0.0.1:0".parse().unwrap();
-    let socket = match UdpSocket::bind(addr) {
+pub async fn any_udp_socket() -> UdpSocket {
+    let socket = match UdpSocket::bind("127.0.0.1:0").await {
         Ok(s) => s,
-        Err(e) => {
-            panic!("error when opening a new mio socket: {}", e);
-        }
+        Err(e) => panic!("error when opening a new mio socket: {}", e),
     };
     socket
 }
