8fe45931d7e84664e15a482fa001618f99a1ef32
dkf <dannzor@outlook.com>
2021-12-06 18:46:30 +0100

    network: add support for server backend to receive messages with root hash

--- src/network/server_handle.rs
+++ src/network/server_handle.rs
@@ -1,13 +1,13 @@
 use super::common::{extract_server_port_field, verify_p2p_message};
 use super::udp_utils::get_udp_and_addr;
-use super::{FromServerEvent, OrdPayload, PayloadEvent, PeerInfo, UpgradedPeerData};
+use super::{FromServerEvent, OrdPayload, PayloadEvent, PeerInfo, PeerShortId, UpgradedPeerData};
 use std::collections::HashMap;
 use std::io;
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use themis::keys::EcdsaPublicKey;
 use tokio::sync::mpsc::{Sender, UnboundedSender};
-use tokio::sync::Notify;
+use tokio::sync::{Mutex, Notify};
 
 pub async fn spawn_server_accept_loop(
     tx_out: Sender<FromServerEvent>,
@@ -15,6 +15,7 @@ pub async fn spawn_server_accept_loop(
     tx2_in: UnboundedSender<UpgradedPeerData>,
     validators: Vec<String>,
     notify: Arc<Notify>,
+    root_hash: String,
 ) {
     let join = tokio::spawn(async move {
         event_loop(tx_out, tx_in, tx2_in, validators, notify).await;
@@ -29,6 +30,7 @@ async fn event_loop(
     tx2_in: UnboundedSender<UpgradedPeerData>,
     validator_list: Vec<String>,
     notify: Arc<Notify>,
+    root_hash: String,
 ) -> Result<(), std::io::Error> {
     const PEER_MESSAGE_MAX_LEN: usize = 248;
     let mut stream_size: Option<usize> = None;
@@ -51,14 +53,18 @@ async fn event_loop(
     let mut upgraded_peers: Vec<String> = Vec::new();
     let mut registry_done = false;
     let mut upgrade_done = false;
+    let mut root_hash_confirmed = false;
+    let mut root_hash_peers: Vec<String> = Vec::new();
+
+    // Public key mapped to short ID: String -> 'usize'
+    let hex_key_to_id = Arc::new(Mutex::new(HashMap::<String, usize>::new()));
 
     notify.notify_one();
 
     let tx_ug = tx2_in.clone();
-    let mut nonce: u32 = 0;
+    let mut nonce: usize = 0;
     loop {
         buf = [0; PEER_MESSAGE_MAX_LEN];
-        // unwrapping here not the most pretty but let is pass for now
         let stream_size = Some(socket.recv(&mut buf).await?);
 
         if let Some(_) = stream_size {
@@ -71,6 +77,7 @@ async fn event_loop(
                 let peer_key = hex::encode(peer_key_type.clone());
                 let in_registered_list = registered_peers.contains(&peer_key);
                 let in_upgraded_list = upgraded_peers.contains(&peer_key);
+                let in_root_list = root_hash_peers.contains(&peers_key);
                 if !validator_list.contains(&peer_key) {
                     log::warn!("peer not in whitelist");
                     continue;
@@ -95,8 +102,12 @@ async fn event_loop(
                     }
 
                     registered_peers.push(peer_key.clone());
+                    let arc = hex_key_to_id.clone();
+                    let mut inner = arc.lock().await;
+                    inner.insert(peer_key.clone(), nonce);
                     log::debug!("register peer with id {} as seen", nonce);
                     registry_done = registered_peers.len() == validator_list.len();
+                    continue;
                 }
                 let (is_valid, peer_id) = is_upgrade_ack_tag(buf.to_vec());
                 if is_valid && !upgrade_done && !in_upgraded_list {
@@ -125,8 +136,26 @@ async fn event_loop(
                 } else {
                     log::error!("upgrade ACK message rejected, discard");
                 }
+                continue;
+            }
+            let (is_valid, peer_id) = is_root_hash_tag(buf.to_vec());
+            if is_valid && !root_hash_confirmed && !in_root_list {
+                let hash = extract_root_hash(buf.to_vec());
+                if hash.is_none() {
+                    log::error!("incorrect root hash, discard message");
+                    continue;
+                }
+                let hash = hash.unwrap();
+                if hash == root_hash {
+                    root_hash_peers.push(peer_key);
+                    log::info!(
+                        "peer {} synced with local root hash, {} left",
+                        peer_id,
+                        validator_list.len() - 1 - root_hash_peers.len(),
+                    )
+                }
             } else {
-                log::debug!("normal mode, wait for consensus message");
+                log::debug!("waiting for consensus message..");
             }
         }
     }
@@ -141,7 +170,7 @@ fn is_upgrade_syn_tag(v: Vec<u8>) -> bool {
 
 fn is_upgrade_ack_tag(v: Vec<u8>) -> (bool, u8) {
     let expected = "ACK=".to_string().as_bytes().to_vec();
-    let is_correct_tag = v[90..94].to_vec() == expected;
+    let is_correct_tag = v[90..90 + expected.len()].to_vec() == expected;
     let dig = v[94]; // digit 48 is '0' and digit 57 is '9'
     let is_num = dig >= 48 && dig <= 57;
     if is_correct_tag && is_num {
@@ -149,4 +178,27 @@ fn is_upgrade_ack_tag(v: Vec<u8>) -> (bool, u8) {
     } else {
         (false, 0)
     }
+}
+
+fn is_root_hash_tag(v: Vec<u8>) -> (bool, u8) {
+    let expected = "RTHASH".to_string().as_bytes().to_vec();
+    let is_correct_tag = v[1..1 + expected.len()].to_vec() == expected;
+    let dig = v[0];
+    let is_num = dig >= 48 && dig <= 57;
+    if is_correct_tag && is_num {
+        (true, dig - 48)
+    } else {
+        (false, 0)
+    }
+}
+
+fn extract_root_hash(v: Vec<u8>) -> Option<String> {
+    let rh_len = 64;
+    let root_hash = &v[7..7 + rh_len];
+    match std::str::from_utf8(root_hash) {
+        Ok(s) => {
+            return Some(s.to_string());
+        }
+        Err(_) => return None,
+    };
 }
