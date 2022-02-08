1839804a5fe478435b964eec4651bfedeafb4ddb
dkf <dannzor@outlook.com>
2022-02-08 20:07:07 +0100

    network: further decoupling between main and test pipes

--- network/src/peer.rs
+++ network/src/peer.rs
@@ -36,9 +36,32 @@ impl Pipes {
     }
 }
 
-pub async fn initiate_ping(
-    rw: &Arc<Mutex<Option<TcpStream>>>,
-    test_w: Option<TestPipeSend>,
+pub async fn peer_handshake_loop(
+    rw: Arc<Mutex<TcpStream>>,
+    other_stream_id: EcdsaPublicKey,
+    test_rcv: Option<broadcast::Receiver<Vec<u8>>>,
+    fetcher: mpsc::Sender<Vec<u8>>,
+    err: mpsc::Sender<String>,
+) {
+    if test_rcv.is_some() {
+        tokio::task::spawn(async move {
+            message_listen_loop_mock(
+                test_rcv.unwrap(),
+                other_stream_id.clone(),
+                fetcher.clone(),
+                err.clone(),
+            )
+            .await;
+        });
+    } else {
+        message_listen_loop(&rw, other_stream_id, fetcher, err).await;
+    }
+}
+
+// Sends a ping message if the peer is supposed to initiate a three-way handshake
+// which is the one with the "highest value" key is the initiator.
+async fn initiate_ping(
+    rw: &Arc<Mutex<TcpStream>>,
     ping_msg: Vec<u8>,
     remote_id: EcdsaPublicKey,
     handshake_id: EcdsaPublicKey,
@@ -49,7 +72,7 @@ pub async fn initiate_ping(
     let priority_id = cmp_two_keys(remote_id, handshake_id.clone());
     if priority_id == handshake_id {
         let tmp = rw.clone();
-        match send(&tmp, test_w, ping_msg).await {
+        match send(&tmp, ping_msg).await {
             Ok(_) => {
                 let mut hs_phase = three_way_handshake_counter.lock().unwrap();
                 *hs_phase = 1;
@@ -65,52 +88,72 @@ pub async fn initiate_ping(
     }
 }
 
-async fn send(
-    rw: &Arc<Mutex<Option<TcpStream>>>,
-    test_w: Option<TestPipeSend>,
-    msg: Vec<u8>,
-) -> Result<(), io::Error> {
+async fn initiate_ping_mock(
+    test_w: TestPipeSend,
+    ping_msg: Vec<u8>,
+    remote_id: EcdsaPublicKey,
+    handshake_id: EcdsaPublicKey,
+    three_way_handshake_counter: Arc<Mutex<usize>>,
+) {
+    let priority_id = cmp_two_keys(remote_id.clone(), handshake_id.clone());
+    if priority_id == handshake_id {
+        match send_mock(test_w, ping_msg).await {
+            Ok(_) => {
+                let mut hs_phase = three_way_handshake_counter.lock().unwrap();
+                *hs_phase = 1;
+            }
+            Err(e) => {
+                log::error!("failed to send ping: {:?}", e);
+            }
+        };
+    } else {
+        return;
+    }
+}
+
+// Writes a message to either a TCP connection or the mock tokio channel
+async fn send(rw: &Arc<Mutex<TcpStream>>, msg: Vec<u8>) -> Result<(), io::Error> {
     let l = msg.len();
     let tmp = rw.clone();
     let rw = tmp.lock().unwrap();
-    if rw.is_some() {
-        loop {
-            let send = rw.as_ref().unwrap();
-            let _ = send.writable().await;
+    loop {
+        let _ = rw.writable().await;
 
-            match send.try_write(&msg) {
-                Ok(n) => {
-                    if n != l {
-                        return Err(io::Error::new(
-                            ErrorKind::BrokenPipe,
-                            "sent incomplete message",
-                        ));
-                    }
-                    break;
+        match rw.try_write(&msg) {
+            Ok(n) => {
+                if n != l {
+                    return Err(io::Error::new(
+                        ErrorKind::BrokenPipe,
+                        "sent incomplete message",
+                    ));
                 }
-                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                Err(e) => {
-                    return Err(e.into());
-                }
+                break;
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
             }
         }
-    } else if test_w.is_some() {
-        let s = test_w.clone().unwrap().w;
-        let _ = s.send(msg);
-    } else {
-        return Err(io::Error::new(
-            ErrorKind::Unsupported,
-            "neither tcp stream nor test pipe exists",
-        ));
     }
 
     Ok(())
 }
 
-pub async fn read_handshake_loop(
-    rw: Arc<Mutex<Option<TcpStream>>>,
+async fn send_mock(
+    test_w: TestPipeSend,
+    ping_msg: Vec<u8>,
+) -> Result<usize, tokio::sync::broadcast::error::SendError<Vec<u8>>> {
+    let s = test_w.clone().w;
+    s.send(ping_msg)
+}
+
+// Receives messages from the fetcher channel (connected to the message listener loop).
+// This function is responsible for managing the full handshake logic: receive and send of
+// handshake messages.
+async fn read_handshake_loop(
+    rw: Arc<Mutex<TcpStream>>,
     test_w: Option<TestPipeSend>,
     handshakes: FixedHandshakes,
     mut updates: mpsc::Receiver<Vec<u8>>,
@@ -122,8 +165,9 @@ pub async fn read_handshake_loop(
     // A peer P is considered _fully upgraded_ with the client C if and only if:
     // 1) C sent a ping to P, P responded with pong, and C sent an ack; or
     // 2) P sent a ping to C, C responded with pong, and P sent an ack
-    // Values:
-    // 0 = has neither sent a ping not received a ping
+    //
+    // Values of the counter:
+    // 0 = has neither sent a ping nor received a ping
     // 1 = sent or received a ping
     // 2 = sent or received a pong
     // 3 = sent or received an ack
@@ -133,15 +177,25 @@ pub async fn read_handshake_loop(
     let priority_id = cmp_two_keys_string(hex::encode(remote_id.clone()), local_id.clone());
     let tmp = rw.clone();
     if priority_id == local_id {
-        initiate_ping(
-            &tmp,
-            test_w.clone(),
-            handshakes.ping(),
-            remote_id.clone(),
-            handshakes.author_id(),
-            three_way_handshake_counter.clone(),
-        )
-        .await;
+        if test_w.is_some() {
+            initiate_ping_mock(
+                test_w.unwrap(),
+                handshakes.ping(),
+                remote_id.clone(),
+                handshakes.author_id(),
+                three_way_handshake_counter.clone(),
+            )
+            .await;
+        } else {
+            initiate_ping(
+                &tmp,
+                handshakes.ping(),
+                remote_id.clone(),
+                handshakes.author_id(),
+                three_way_handshake_counter.clone(),
+            )
+            .await;
+        }
     }
 
     loop {
@@ -149,8 +203,8 @@ pub async fn read_handshake_loop(
             let mut hs_phase = three_way_handshake_counter.lock().unwrap();
             let has_init = initiated_handshake.lock().unwrap();
 
-            // all messages sent on this channel has already been checked with `from_handshake` and
-            // are error-free hence the unwrap
+            // all messages sent on this channel have already been checked with `from_handshake` once
+            // before arriving here and are thus error-free, hence the immediate unwrap
             let parsed_msg = from_handshake(msg, remote_id.clone()).unwrap();
 
             // this peer has neither received a ping nor sent one
@@ -159,7 +213,7 @@ pub async fn read_handshake_loop(
                     *hs_phase = 1;
                     let pong_msg = handshakes.pong();
                     let tmp = rw.clone();
-                    match send(&tmp, test_w.clone(), pong_msg).await {
+                    match send(&tmp, pong_msg).await {
                         Ok(_) => {
                             *hs_phase = 2;
                         }
@@ -179,7 +233,7 @@ pub async fn read_handshake_loop(
                 if parsed_msg.code() == "pong" {
                     let ack_msg = handshakes.ack();
                     let tmp = rw.clone();
-                    match send(&tmp, test_w.clone(), ack_msg).await {
+                    match send(&tmp, ack_msg).await {
                         Ok(_) => {
                             *hs_phase = 3;
                         }
@@ -199,60 +253,63 @@ pub async fn read_handshake_loop(
     }
 }
 
+// Listens for p2p messages from either the TCP stream or the tokio channel. It then relays these
+// messages to the fetcher.
 async fn message_listen_loop(
-    stream: Option<Arc<Mutex<TcpStream>>>,
-    stream_id: EcdsaPublicKey,
-    test_rcv: Option<Arc<Mutex<mpsc::Receiver<Vec<u8>>>>>,
+    rw: &Arc<Mutex<TcpStream>>,
+    other_stream_id: EcdsaPublicKey,
     fetcher: mpsc::Sender<Vec<u8>>,
     err: mpsc::Sender<String>,
 ) {
     const MAX_LENGTH: usize = 400;
-    if stream.is_some() {
-        loop {
-            let s = stream.as_ref().unwrap().lock().unwrap();
-            let _ = s.readable().await;
-            let mut buf = [0; MAX_LENGTH];
+    let arc = rw.clone();
+    let rw = arc.lock().unwrap();
+    loop {
+        let _ = rw.readable().await;
+        let mut buf = [0; MAX_LENGTH];
 
-            match s.try_read(&mut buf) {
-                Ok(0) => {
-                    continue;
-                }
-                Ok(_) => {
-                    let message_vec = buf.to_vec();
-                    let _ = match from_handshake(message_vec.clone(), stream_id.clone()) {
-                        Ok(_) => {}
-                        Err(e) => {
-                            let _ = err.send(e.to_string()).await;
-                            continue;
-                        }
-                    };
-                    let _ = fetcher.send(message_vec).await;
-                }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                Err(e) => {
-                    let _ = err.send(e.to_string()).await;
-                }
+        match rw.try_read(&mut buf) {
+            Ok(0) => {
+                continue;
             }
-        }
-    } else if test_rcv.is_some() {
-        loop {
-            let arc = test_rcv.clone().unwrap();
-            let mut read = arc.lock().unwrap();
-            while let Some(rx_msg) = read.recv().await {
-                let _ = match from_handshake(rx_msg.clone(), stream_id.clone()) {
+            Ok(_) => {
+                let message_vec = buf.to_vec();
+                let _ = match from_handshake(message_vec.clone(), other_stream_id.clone()) {
                     Ok(_) => {}
                     Err(e) => {
                         let _ = err.send(e.to_string()).await;
                         continue;
                     }
                 };
-                let _ = fetcher.send(rx_msg).await;
+                let _ = fetcher.send(message_vec).await;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                let _ = err.send(e.to_string()).await;
             }
         }
-    } else {
-        let _ = err.send("missing pipe(s), exit".to_string()).await;
+    }
+}
+
+async fn message_listen_loop_mock(
+    mut test_rcv: broadcast::Receiver<Vec<u8>>,
+    other_stream_id: EcdsaPublicKey,
+    fetcher: mpsc::Sender<Vec<u8>>,
+    err: mpsc::Sender<String>,
+) {
+    loop {
+        while let Ok(rx_msg) = test_rcv.recv().await {
+            let _ = match from_handshake(rx_msg.clone(), other_stream_id.clone()) {
+                Ok(_) => {}
+                Err(e) => {
+                    let _ = err.send(e.to_string()).await;
+                    continue;
+                }
+            };
+            let _ = fetcher.clone().send(rx_msg).await;
+        }
     }
 }
 
@@ -320,10 +377,5 @@ mod tests {
     use themis::keygen;
 
     #[tokio::test]
-    async fn full_three_way_handshakes_between_two_peers() {
-        let (a_sk, a_pk) = keygen::gen_ec_key_pair().split();
-        let (b_sk, b_pk) = keygen::gen_ec_key_pair().split();
-        let a_hs = FixedHandshakes::new(a_pk.clone(), "8080".to_string(), a_sk).unwrap();
-        let b_hs = FixedHandshakes::new(b_pk.clone(), "8081".to_string(), b_sk).unwrap();
-    }
+    async fn full_three_way_handshakes_between_two_peers() {}
 }
