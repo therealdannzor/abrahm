a05d97d1341a858bb125ef865d3b544f8706073c
dkf <dannzor@outlook.com>
2022-03-14 17:09:37 +0100

    consensus: update test helper

--- consensus/src/testcommons.rs
+++ consensus/src/testcommons.rs
@@ -1,12 +1,15 @@
 #![allow(unused)]
 use themis::keygen;
+use themis::keys::EcdsaPublicKey;
 
-pub fn generate_keys_as_str(amount: u8) -> Vec<String> {
-    let mut result = Vec::new();
+pub fn generate_keys_as_str_and_type(amount: u8) -> (Vec<String>, Vec<EcdsaPublicKey>) {
+    let mut string_result = Vec::new();
+    let mut type_result = Vec::new();
     for _ in 0..amount {
         let (_, pk) = keygen::gen_ec_key_pair().split();
+        type_result.push(pk.clone());
         let pk: String = hex::encode(pk);
-        result.push(pk);
+        string_result.push(pk);
     }
-    result
+    (string_result, type_result)
 }
